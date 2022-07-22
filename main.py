cf1441ff9f3573e6ac193c2a172b258f3b73e076
Nikhil-987651 <109678700+Nikhil-987651@users.noreply.github.com>
2022-07-22 12:55:24 +0530

    Assignment file added

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.header('Enter two numbers to multiply them')
+st.write("")
+
+
+def user_input():
+    number_1 = st.number_input("Enter the First Number")
+    st.write("")
+    number_2 = st.number_input("Enter the Second Number")
+    return number_1, number_2
+
+
+st.write("")
+st.write("")
+
+numbers = user_input()
+
+st.header(str(numbers[0]) + "  *  " + str(numbers[1]) + "  =  " + str(numbers[0]*numbers[1]))
+#print(numbers)
+
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
